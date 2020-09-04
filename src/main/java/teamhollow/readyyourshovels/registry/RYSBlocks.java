a39c4a63fff81cbf94704f94b2612bd8a4718771
pentantan <shutux500@gmail.com>
2020-09-05 06:27:40 +0900

    first forge commit

---
+++ src/main/java/teamhollow/readyyourshovels/registry/RYSBlocks.java
@@ -0,0 +1,8 @@
+package teamhollow.readyyourshovels.registry;
+
+import net.minecraftforge.fml.common.Mod;
+import teamhollow.readyyourshovels.ReadyYourShovelsCore;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = ReadyYourShovelsCore.MODID)
+public class RYSBlocks {
+}

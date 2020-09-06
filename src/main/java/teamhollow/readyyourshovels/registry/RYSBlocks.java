327a6ebdea41f83a809f6c7b1412947034c75449
pentantan <shutux500@gmail.com>
2020-09-06 12:04:43 +0900

    dirt ore things

--- src/main/java/teamhollow/readyyourshovels/registry/RYSBlocks.java
+++ src/main/java/teamhollow/readyyourshovels/registry/RYSBlocks.java
@@ -14,17 +14,25 @@ import teamhollow.readyyourshovels.ReadyYourShovelsCore;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = ReadyYourShovelsCore.MODID)
 public class RYSBlocks {
+    public static final Block CLAY_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
     public static final Block DIRT_BRICK = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+    public static final Block GOLD_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(2.0F, 2.5F).sound(SoundType.GROUND));
+    public static final Block IRON_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.5F, 2.0F).sound(SoundType.GROUND));
 
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> registry) {
+        registry.getRegistry().register(CLAY_DEPOSIT.setRegistryName("clay_deposit"));
         registry.getRegistry().register(DIRT_BRICK.setRegistryName("dirt_bricks"));
+        registry.getRegistry().register(GOLD_DEPOSIT.setRegistryName("gold_deposit"));
+        registry.getRegistry().register(IRON_DEPOSIT.setRegistryName("iron_deposit"));
     }
 
     @SubscribeEvent
     public static void registerItemBlocks(RegistryEvent.Register<Item> registry) {
-
+        RYSItems.register(registry, new BlockItem(CLAY_DEPOSIT, (new Item.Properties()).group(ItemGroup.BUILDING_BLOCKS)));
         RYSItems.register(registry, new BlockItem(DIRT_BRICK, (new Item.Properties()).group(ItemGroup.BUILDING_BLOCKS)));
+        RYSItems.register(registry, new BlockItem(GOLD_DEPOSIT, (new Item.Properties()).group(ItemGroup.BUILDING_BLOCKS)));
+        RYSItems.register(registry, new BlockItem(IRON_DEPOSIT, (new Item.Properties()).group(ItemGroup.BUILDING_BLOCKS)));
     }
 }

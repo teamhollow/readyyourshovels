d61391b522d57bb6c9ccfd7921543d5940b9f91c
pentantan <shutux500@gmail.com>
2020-09-12 12:49:21 +0900

    add 3 dirt stairs

--- src/main/java/teamhollow/readyyourshovels/registry/RYSBlocks.java
+++ src/main/java/teamhollow/readyyourshovels/registry/RYSBlocks.java
@@ -2,6 +2,7 @@ package teamhollow.readyyourshovels.registry;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -15,23 +16,36 @@ import teamhollow.readyyourshovels.ReadyYourShovelsCore;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = ReadyYourShovelsCore.MODID)
 public class RYSBlocks {
     public static final Block CLAY_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+
     public static final Block DIRT_BRICK = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+    public static final Block DIRT_BRICK_STAIRS = new StairsBlock(DIRT_BRICK::getDefaultState, Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+
     public static final Block GOLD_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(2).hardnessAndResistance(2.0F, 2.5F).sound(SoundType.GROUND));
     public static final Block IRON_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(1).hardnessAndResistance(1.5F, 2.0F).sound(SoundType.GROUND));
     public static final Block PEAT_DEPOSIT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+
     public static final Block SMOOTH_DIRT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+    public static final Block SMOOTH_DIRT_STAIRS = new StairsBlock(SMOOTH_DIRT::getDefaultState, Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+
     public static final Block TOUGH_DIRT = new Block(Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
+    public static final Block TOUGH_DIRT_STAIRS = new StairsBlock(TOUGH_DIRT::getDefaultState, Block.Properties.create(Material.EARTH).harvestTool(ToolType.SHOVEL).setRequiresTool().harvestLevel(0).hardnessAndResistance(1.0F, 1.5F).sound(SoundType.GROUND));
 
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> registry) {
         registry.getRegistry().register(CLAY_DEPOSIT.setRegistryName("clay_deposit"));
+
         registry.getRegistry().register(DIRT_BRICK.setRegistryName("dirt_bricks"));
+        registry.getRegistry().register(DIRT_BRICK_STAIRS.setRegistryName("dirt_bricks_stairs"));
+
         registry.getRegistry().register(GOLD_DEPOSIT.setRegistryName("gold_deposit"));
         registry.getRegistry().register(IRON_DEPOSIT.setRegistryName("iron_deposit"));
         registry.getRegistry().register(PEAT_DEPOSIT.setRegistryName("peat_deposit"));
+
         registry.getRegistry().register(SMOOTH_DIRT.setRegistryName("smooth_dirt"));
+        registry.getRegistry().register(SMOOTH_DIRT_STAIRS.setRegistryName("smooth_dirt_stairs"));
         registry.getRegistry().register(TOUGH_DIRT.setRegistryName("tough_dirt"));
+        registry.getRegistry().register(TOUGH_DIRT_STAIRS.setRegistryName("tough_dirt_stairs"));
     }
 
     @SubscribeEvent

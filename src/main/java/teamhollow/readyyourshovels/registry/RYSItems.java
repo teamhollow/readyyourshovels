25ea17448665dfbc76893710e6d14120a28f5895
pentantan <shutux500@gmail.com>
2020-09-16 17:59:54 +0900

    add peat

--- src/main/java/teamhollow/readyyourshovels/registry/RYSItems.java
+++ src/main/java/teamhollow/readyyourshovels/registry/RYSItems.java
@@ -2,13 +2,18 @@ package teamhollow.readyyourshovels.registry;
 
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import teamhollow.readyyourshovels.ReadyYourShovelsCore;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = ReadyYourShovelsCore.MODID)
 public class RYSItems {
+    public static final Item PEAT = new Item(new Item.Properties().group(ItemGroup.MISC));
+
+
     public static void register(RegistryEvent.Register<Item> registry, Item item, String id) {
         if (item instanceof BlockItem) {
             Item.BLOCK_TO_ITEM.put(((BlockItem) item).getBlock(), item);
@@ -28,5 +33,10 @@ public class RYSItems {
         }
 
         registry.getRegistry().register(item);
+    }
+
+    @SubscribeEvent
+    public static void registerItem(RegistryEvent.Register<Item> registry) {
+        registry.getRegistry().register(PEAT.setRegistryName("peat"));
     }
 }

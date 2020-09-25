7785d5df2403f31102a9e038df2e204b0d3b63a5
AndanteDevs <orrdaniel73@gmail.com>
2020-09-25 07:37:23 +0100

    Cobblestone to Mossy Cobblestone in dirt caves

--- src/main/java/net/teamhollow/readyyourshovels/ReadyYourShovels.java
+++ src/main/java/net/teamhollow/readyyourshovels/ReadyYourShovels.java
@@ -63,7 +63,7 @@ public class ReadyYourShovels implements ModInitializer {
             if ((biome.getCategory() != Biome.Category.NETHER && biome.getCategory() != Biome.Category.THEEND) && biome.getCategory() != Biome.Category.OCEAN && biome.getCategory() != Biome.Category.DESERT && biome.getCategory() != Biome.Category.BEACH && biome.getCategory() != Biome.Category.EXTREME_HILLS) {
                 biome.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, RYSFeatures.DIRT_SURFACE_FEATURE);
 
-                biome.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, RYSFeatures.ORE.configure(new RYSOreFeatureConfig(RYSOreFeatureConfig.Target.TOUGH_DIRT, Blocks.COBBLESTONE.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(5, 0, 0, 80))));
+                biome.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, RYSFeatures.ORE.configure(new RYSOreFeatureConfig(RYSOreFeatureConfig.Target.TOUGH_DIRT, Blocks.MOSSY_COBBLESTONE.getDefaultState(), 33)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(5, 0, 0, 80))));
 
                 biome.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, RYSFeatures.ORE.configure(new RYSOreFeatureConfig(RYSOreFeatureConfig.Target.TOUGH_DIRT, RYSBlocks.CLAY_DEPOSIT.getDefaultState(), 9)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(18, 0, 0, 128))));
                 biome.addFeature(GenerationStep.Feature.UNDERGROUND_DECORATION, RYSFeatures.ORE.configure(new RYSOreFeatureConfig(RYSOreFeatureConfig.Target.TOUGH_DIRT, RYSBlocks.PEAT_DEPOSIT.getDefaultState(), 12)).createDecoratedFeature(Decorator.COUNT_RANGE.configure(new RangeDecoratorConfig(20, 0, 0, 128))));

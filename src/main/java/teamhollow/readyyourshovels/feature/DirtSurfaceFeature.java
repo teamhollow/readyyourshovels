be26e0dedbb00476b86eca4512d43947b2f46485
shutux <shutux500@gmail.com>
2020-12-19 15:28:00 +0900

    add gardenAnt's anthill gen

--- src/main/java/teamhollow/readyyourshovels/feature/DirtSurfaceFeature.java
+++ src/main/java/teamhollow/readyyourshovels/feature/DirtSurfaceFeature.java
@@ -1,15 +1,21 @@
 package teamhollow.readyyourshovels.feature;
 
 import com.mojang.serialization.Codec;
+import net.minecraft.block.AirBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.ISeedReader;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
+import teamhollow.readyyourshovels.block.AntNestBlock;
+import teamhollow.readyyourshovels.entity.GardenAntEntity;
 import teamhollow.readyyourshovels.registry.RYSBlocks;
+import teamhollow.readyyourshovels.registry.RYSEntity;
+import teamhollow.readyyourshovels.tileentity.AntNestTileEntity;
 
 import java.util.Random;
 
@@ -22,25 +28,47 @@ public class DirtSurfaceFeature extends Feature<NoFeatureConfig> {
     public boolean generate(ISeedReader world, ChunkGenerator chunkGenerator, Random random, BlockPos pos, NoFeatureConfig config) {
         BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
 
-        for (int i = 0; i < 16; ++i) {
-            for (int j = 0; j < 16; ++j) {
-                int k = pos.getX() + i;
-                int l = pos.getZ() + j;
-                int i1 = world.getHeight(Heightmap.Type.WORLD_SURFACE_WG, k, l);
+        for (int x = 0; x < 16; ++x) {
+            for (int z = 0; z < 16; ++z) {
+                int posX = pos.getX() + x;
+                int posZ = pos.getZ() + z;
+                int posY = world.getHeight(Heightmap.Type.WORLD_SURFACE_WG, posX, posZ);
 
                 for (int d = 0; d < 28; ++d) {
-                    blockpos$mutable.setPos(k, i1 - d, l);
+                    blockpos$mutable.setPos(posX, posY - d, posZ);
 
                     BlockState state = world.getBlockState(blockpos$mutable);
                     if (state.isIn(Blocks.STONE) || state.isIn(Blocks.GRANITE) || state.isIn(Blocks.DIORITE) || state.isIn(Blocks.ANDESITE) || state.isIn(Blocks.COAL_ORE) || state.isIn(Blocks.IRON_ORE) || state.isIn(Blocks.GOLD_ORE)) {
                         if (d >= 27) {
                             world.setBlockState(blockpos$mutable, RYSBlocks.REGOLITH.getDefaultState(), 2);
                         } else {
+                            if (random.nextFloat() < 0.0075F && neigboringAreFree(blockpos$mutable.up(), world)) {
+                                world.setBlockState(blockpos$mutable.up(), RYSBlocks.ANT_NEST.getDefaultState().with(AntNestBlock.FACING, AntNestBlock.getGenerationDirection(random)), 2);
+                                TileEntity tileEntity = world.getTileEntity(blockpos$mutable.up());
+                                if (tileEntity instanceof TileEntity) {
+                                    AntNestTileEntity antNestBlockEntity = (AntNestTileEntity) tileEntity;
+                                    int rand = 2 + random.nextInt(2);
+
+                                    for (int i = 0; i < rand; ++i) {
+                                        GardenAntEntity gardenAntEntity = new GardenAntEntity(RYSEntity.GARDEN_ANT, world.getWorld());
+                                        antNestBlockEntity.tryEnterNest(gardenAntEntity, false, random.nextInt(599));
+                                    }
+                                }
+                            }
+
                             world.setBlockState(blockpos$mutable, RYSBlocks.TOUGH_DIRT.getDefaultState(), 2);
                         }
                     }
                 }
             }
+        }
+
+        return true;
+    }
+
+    private boolean neigboringAreFree(BlockPos pos, ISeedReader world) {
+        for (BlockPos i : new BlockPos[]{pos, pos.up(), pos.north(), pos.east(), pos.south(), pos.west()}) {
+            if (!(world.getBlockState(i).getBlock() instanceof AirBlock)) return false;
         }
 
         return true;

88a863c4b7cd076c521ed60b48165aa0cf5c8322
Daniel Orr <orrdaniel73@gmail.com>
2020-12-22 14:18:56 +0000

    port 1.14 rys's ant animations

--- src/main/java/net/teamhollow/readyyourshovels/entity/garden_ant/GardenAntEntityModel.java
+++ src/main/java/net/teamhollow/readyyourshovels/entity/garden_ant/GardenAntEntityModel.java
@@ -2,10 +2,13 @@ package net.teamhollow.readyyourshovels.entity.garden_ant;
 
 import com.google.common.collect.ImmutableList;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.entity.model.CompositeEntityModel;
 import net.minecraft.util.math.MathHelper;
 
+@Environment(EnvType.CLIENT)
 public class GardenAntEntityModel<T extends GardenAntEntity> extends CompositeEntityModel<T> {
     private final ModelPart thorax;
     private final ModelPart head;
@@ -87,42 +90,22 @@ public class GardenAntEntityModel<T extends GardenAntEntity> extends CompositeEn
 
     @Override
     public void setAngles(T entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
-        this.head.yaw = netHeadYaw * 0.017453292F;
-        this.head.pitch = headPitch * 0.017453292F;
+        limbSwing /= 2;
 
-        this.rightFrontLeg.pivotZ = -0.75F;
-        this.leftFrontLeg.pivotZ = -0.75F;
-        this.rightMidLeg.pivotZ = -0.25F;
-        this.leftMidLeg.pivotZ = 0.5F;
-        this.rightBackLeg.pivotZ = 0.75F;
-        this.leftBackLeg.pivotZ = 0.5F;
+        head.pitch = headPitch * (((float) Math.PI) / 180.0F);
+        head.yaw = netHeadYaw * (((float) Math.PI) / 180.0F);
 
-        this.rightFrontLeg.pivotY = 21.5F;
-        this.leftFrontLeg.pivotY = 21.5F;
-        this.rightMidLeg.pivotY = 21.5F;
-        this.leftMidLeg.pivotY = 21.5F;
-        this.rightBackLeg.pivotY = 21.5F;
-        this.leftBackLeg.pivotY = 21.5F;
-        float f3 = -(MathHelper.cos(limbSwing * 1.6662F * 2.0F + 0.0F) * 0.6F) * limbSwingAmount;
-        float f4 = -(MathHelper.cos(limbSwing * 1.6662F * 2.0F + (float) Math.PI) * 0.6F) * limbSwingAmount;
-        float f5 = -(MathHelper.cos(limbSwing * 1.6662F * 2.0F + ((float) Math.PI / 2F)) * 0.6F) * limbSwingAmount;
-        float f6 = -(MathHelper.cos(limbSwing * 1.6662F * 2.0F + ((float) Math.PI * 1.5F)) * 0.6F) * limbSwingAmount;
-        float f7 = 2 * (MathHelper.sin(limbSwing * 1.6662F * (1.0F)) * 0.6F) * limbSwingAmount;
-        float f8 = 2 * (MathHelper.sin(limbSwing * 1.6662F + 0.0F) * 0.6F) * limbSwingAmount;
-        float f9 = 2 * (MathHelper.sin(limbSwing * 1.6662F + 0.0F) * 0.6F) * limbSwingAmount;
-        float f10 = 2 * (MathHelper.sin(limbSwing * 1.6662F + 0.0F) * 0.6F) * limbSwingAmount;
-        this.rightFrontLeg.pivotY += f3;
-        this.leftFrontLeg.pivotY += -f3;
-        this.rightMidLeg.pivotY += f4;
-        this.leftMidLeg.pivotY += -f5;
-        this.rightBackLeg.pivotY += f6;
-        this.leftBackLeg.pivotY += -f6;
-        this.rightFrontLeg.pivotZ += f7;
-        this.leftFrontLeg.pivotZ += f7;
-        this.rightMidLeg.pivotZ += -f8;
-        this.leftMidLeg.pivotZ += -f9;
-        this.rightBackLeg.pivotZ += f10;
-        this.leftBackLeg.pivotZ += f10;
+        setRotationAngle(antennaLeft, 0.5236F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.0F, (MathHelper.sin((limbSwing * 4.00F)) * (0.125F)) + 0.0F);
+        setRotationAngle(antennaRight, 0.5236F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.0F, (MathHelper.sin((limbSwing * 4.00F)) * (0.125F)) + 0.0F);
+        setRotationAngle(abdomen, -0.1745F, 0.0F, 0.0F);
+
+        setRotationAngle(rightFrontLeg, 0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) + 0.5236F, (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) + 0.5236F);
+        setRotationAngle(rightMidLeg, 0.0F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) + 0.0F   , (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) + 0.5236F);
+        setRotationAngle(rightBackLeg, 0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.5236F, (MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) + 0.5236F);
+
+        setRotationAngle(leftFrontLeg, 0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.5236F, (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) - 0.5236F);
+        setRotationAngle(leftMidLeg, 0.0F, MathHelper.sin((limbSwing * 4.00F)) * (0.125F) - 0.0F   , (-MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) - 0.5236F);
+        setRotationAngle(leftBackLeg, 0.0F, -MathHelper.sin((limbSwing * 4.00F)) * (0.125F) + 0.5236F, (MathHelper.sin((limbSwing * 4.00F)) * (0.25F)) - 0.5236F);
     }
 
     @Override
--- src/main/java/net/teamhollow/readyyourshovels/entity/garden_ant/GardenAntEntityRenderer.java
+++ src/main/java/net/teamhollow/readyyourshovels/entity/garden_ant/GardenAntEntityRenderer.java
@@ -1,11 +1,14 @@
 package net.teamhollow.readyyourshovels.entity.garden_ant;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import net.teamhollow.readyyourshovels.init.RYSEntities;
 
+@Environment(EnvType.CLIENT)
 public class GardenAntEntityRenderer extends MobEntityRenderer<GardenAntEntity, GardenAntEntityModel<GardenAntEntity>> {
     public GardenAntEntityRenderer(EntityRenderDispatcher entityRenderDispatcher) {
         super(entityRenderDispatcher, new GardenAntEntityModel<GardenAntEntity>(), 0.25F);
